0729bbda078fc04e4f7a694b9ed5dc5999de64f8
raiguard <caleb.heuer@gmail.com>
2021-04-08 17:26:18 -0600

    Collect mods input into a struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::fs;
+use std::ops::Deref;
 use std::path::PathBuf;
 
 #[derive(Debug)]
 struct AppArgs {
-    enable: Option<Vec<ModIdentifier>>,
+    // TODO: `enable` and `disable` can be combined
+    enable: Option<ModsInputList>,
     enable_all: bool,
-    disable: Option<Vec<ModIdentifier>>,
+    disable: Option<ModsInputList>,
     disable_all: bool,
     mods_path: String,
 }
@@ -16,18 +18,56 @@ impl AppArgs {
     fn new(mut pargs: pico_args::Arguments) -> Result<AppArgs, pico_args::Error> {
         Ok(AppArgs {
             disable_all: pargs.opt_value_from_str("--disable-all")?.unwrap_or(false),
-            disable: pargs.opt_value_from_fn("--disable", parse_mod_input)?,
+            disable: pargs
+                .opt_value_from_fn("--disable", |value| ModsInputList::new(value, false))?,
             enable_all: pargs.opt_value_from_str("--enable-all")?.unwrap_or(false),
-            enable: pargs.opt_value_from_fn("--enable", parse_mod_input)?,
+            enable: pargs.opt_value_from_fn("--enable", |value| ModsInputList::new(value, true))?,
             mods_path: pargs.value_from_str("--modspath")?,
         })
     }
 }
 
-#[derive(Debug, PartialEq)]
-enum ModIdentifier {
-    Latest(String),
-    Versioned(String, String),
+#[derive(Debug)]
+struct ModsInputList(Vec<ModData>);
+
+impl ModsInputList {
+    fn new(input: &str, to_enable: bool) -> Result<Self, String> {
+        // TODO: Throw error on illegal characters detected
+        // Legal characters are [a-zA-Z0-9_\- ]
+        let mods = input
+            .split('|')
+            .map(|mod_identifier| {
+                let parts: Vec<&str> = mod_identifier.split('@').collect();
+                // TODO: qualify mod name somehow
+                // TODO: Check for duplicates
+
+                if parts.len() == 0 || parts.len() > 2 {
+                    // TODO: More helpful formatting
+                    return Err("Invalid number of mod input sections".to_string());
+                }
+
+                let name = parts.get(0).unwrap();
+                let version = parts.get(1).map(|version| version.to_string());
+
+                Ok(ModData {
+                    name: name.to_string(),
+                    enabled: to_enable,
+                    version,
+                })
+            })
+            .collect::<Result<Vec<ModData>, String>>()?;
+
+        Ok(ModsInputList(mods))
+    }
+}
+
+// Use the ModsInputList like a vector by dereferencing
+impl Deref for ModsInputList {
+    type Target = Vec<ModData>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -51,32 +91,11 @@ impl ModsDirectory {
     }
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 struct ModData {
     name: String,
     enabled: bool,
     version: Option<String>,
-}
-
-fn parse_mod_input(input: &str) -> Result<Vec<ModIdentifier>, String> {
-    // TODO: Throw error on illegal characters detected
-    // Legal characters are [a-zA-Z0-9_\- ]
-    input
-        .split('|')
-        .map(|mod_identifier| {
-            let parts: Vec<&str> = mod_identifier.split('@').collect();
-            // TODO: qualify mod name somehow
-            // TODO: Check for duplicates
-            match parts[..] {
-                [mod_name] => Ok(ModIdentifier::Latest(mod_name.to_string())),
-                [mod_name, mod_version] => Ok(ModIdentifier::Versioned(
-                    mod_name.to_string(),
-                    mod_version.to_string(),
-                )),
-                _ => Err("Invalid mod identifier format".to_string()),
-            }
-        })
-        .collect::<Result<Vec<ModIdentifier>, String>>()
 }
 
 pub fn run(pargs: pico_args::Arguments) -> Result<(), Box<dyn Error>> {
@@ -97,18 +116,34 @@ mod tests {
 
     #[test]
     fn one_latest() {
+        let mods = ModsInputList::new("RecipeBook", true).unwrap();
         assert_eq!(
-            parse_mod_input("RecipeBook"),
-            Ok(vec![ModIdentifier::Latest("RecipeBook".to_string())])
+            mods[0],
+            ModData {
+                name: "RecipeBook".to_string(),
+                enabled: true,
+                version: None
+            }
+        );
+    }
+
+    #[test]
+    fn one_versioned() {
+        let mods = ModsInputList::new("RecipeBook@1.0.0", true).unwrap();
+        assert_eq!(
+            mods[0],
+            ModData {
+                name: "RecipeBook".to_string(),
+                enabled: true,
+                version: Some("1.0.0".to_string()),
+            }
         )
     }
 
     #[test]
     fn invalid_format() {
-        assert_eq!(
-            parse_mod_input("RecipeBook@1.2.3|Foo@bar@set"),
-            Err("Invalid mod identifier format".to_string())
-        )
+        let mods = ModsInputList::new("RecipeBook@1.0.0@foo", true);
+        assert!(mods.is_err());
     }
 
     #[test]
