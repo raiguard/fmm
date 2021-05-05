63e962a9d948a18b1ed6eff35c59c82c65886e6b
raiguard <caleb.heuer@gmail.com>
2021-05-04 18:53:06 -0600

    Change lazy_static for once_cell

--- src/dependency.rs
+++ src/dependency.rs
@@ -1,3 +1,4 @@
+use once_cell::sync::OnceCell;
 use regex::Regex;
 use semver::VersionReq;
 use std::error::Error;
@@ -12,13 +13,15 @@ pub struct ModDependency {
 impl ModDependency {
     pub fn new(input: &str) -> Result<Self, Box<dyn Error>> {
         // Avoid creating the regex object every time
-        lazy_static! {
-            static ref RE: Regex = Regex::new(
-                r"^(?:(?P<type>[!?~]|\(\?\)) *)?(?P<name>(?: *[a-zA-Z0-9_-]+)+(?: *$)?)(?: *(?P<version_req>[<>=]=? *(?:\d+\.){1,2}\d+))?$",
-            ).unwrap();
-        }
-
-        let captures = RE.captures(input).ok_or("Invalid dependency string")?;
+        static RE: OnceCell<Regex> = OnceCell::new();
+        let captures = RE
+            .get_or_init(|| {
+                Regex::new(
+                    r"^(?:(?P<type>[!?~]|\(\?\)) *)?(?P<name>(?: *[a-zA-Z0-9_-]+)+(?: *$)?)(?: *(?P<version_req>[<>=]=? *(?:\d+\.){1,2}\d+))?$",
+                ).unwrap()
+            })
+            .captures(input)
+            .ok_or("Invalid dependency string")?;
 
         Ok(Self {
             dep_type: match captures.name("type").map(|mtch| mtch.as_str()) {
