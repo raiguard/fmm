f82baff3e0050590dcdc94f439652d37c7fc3917
raiguard <caleb.heuer@gmail.com>
2021-06-13 20:29:27 -0600

    Extract info.jsons from all mods

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-use semver::Version;
+use semver::{Version, VersionReq};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::error::Error;
+use std::ffi::OsStr;
 use std::fmt;
+use std::fs;
+use std::fs::{DirEntry, File};
+use std::io::Read;
 use std::path::PathBuf;
 use structopt::StructOpt;
+use zip::ZipArchive;
 
 // TODO: Figure out why it's not coloring the help info.
 #[derive(Debug, StructOpt)]
@@ -14,8 +22,9 @@ struct App {
     #[structopt(short, long)]
     disable: Vec<InputMod>,
     /// The path to the mods directory
+    // TODO: Make optional, introduce config file to specify default path
     #[structopt(short = "f", long)]
-    dir: Option<PathBuf>,
+    dir: PathBuf,
     /// A list of mods to enable. TODO: explain format.
     #[structopt(short, long)]
     enable: Vec<InputMod>,
@@ -24,7 +33,7 @@ struct App {
 #[derive(Debug)]
 struct InputMod {
     name: String,
-    version: ModVersion,
+    version: InputModVersion,
 }
 
 impl std::str::FromStr for InputMod {
@@ -35,7 +44,7 @@ impl std::str::FromStr for InputMod {
         match parts[..] {
             [name] => Ok(Self {
                 name: name.to_string(),
-                version: ModVersion::Latest,
+                version: InputModVersion::Latest,
             }),
             [name, version] => {
                 let parsed_version = Version::parse(version);
@@ -46,7 +55,7 @@ impl std::str::FromStr for InputMod {
                     } else {
                         Ok(Self {
                             name: name.to_string(),
-                            version: ModVersion::Specific(version),
+                            version: InputModVersion::Version(version),
                         })
                     }
                 } else {
@@ -84,13 +93,111 @@ impl fmt::Display for InputModErr {
 }
 
 #[derive(Debug)]
-enum ModVersion {
+enum InputModVersion {
     Latest,
-    Specific(Version),
+    Version(Version),
+}
+
+struct ModsSet {
+    dir: PathBuf,
+    mods: HashMap<String, Mod>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct ModListJson {
+    mods: Vec<ModListJsonMod>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct ModListJsonMod {
+    name: String,
+    version: Option<Version>,
+    enabled: bool,
+}
+
+#[derive(Deserialize, Debug)]
+struct InfoJson {
+    dependencies: Option<Vec<String>>,
+    name: String,
+    version: Version,
+}
+
+impl ModsSet {
+    pub fn new(path: &PathBuf) -> Result<(), Box<dyn Error>> {
+        // // Read mod-list.json to a file
+        // let mut mlj_path = path.clone();
+        // mlj_path.push("mod-list.json");
+        // let mlj_contents = std::fs::read_to_string(mlj_path)?;
+        // let mod_list_json: ModListJson = serde_json::from_str(&mlj_contents)?;
+
+        // Do one thing if it's a symlink or a directory, and another if it's a zip file
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
+            let path = entry.path();
+            let extension = path.extension();
+            let info: InfoJson = if extension.is_some() && extension.unwrap() == OsStr::new("zip") {
+                find_info_json_in_zip(entry)
+            } else {
+                let file_type = entry.file_type()?;
+                if file_type.is_symlink() || file_type.is_dir() {
+                    let mut path = entry.path();
+                    path.push("info.json");
+                    let contents = fs::read_to_string(path)?;
+                    let json: InfoJson = serde_json::from_str(&contents)?;
+                    Ok(json)
+                } else {
+                    Err("Could not find an info.json file".into())
+                }
+            }?;
+            println!("{:#?}", info);
+        }
+
+        Ok(())
+    }
+}
+
+fn find_info_json_in_zip(entry: DirEntry) -> Result<InfoJson, Box<dyn Error>> {
+    let file = File::open(entry.path())?;
+    let mut archive = ZipArchive::new(file)?;
+    // My hand is forced due to the lack of a proper iterator API in the `zip` crate
+    // Thus, we need to use a bare `for` loop and iterate the indices, then act on the file if we find it
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        if file.name().contains("info.json") {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)?;
+            let json: InfoJson = serde_json::from_str(&contents)?;
+            return Ok(json);
+        }
+    }
+    Err("Mod ZIP does not contain an info.json file".into())
+}
+
+struct Mod {
+    name: String,
+    versions: Vec<ModVersion>,
+    enabled: ModEnabledType,
+}
+
+enum ModEnabledType {
+    Disabled,
+    Latest,
+    Version(Version),
+}
+
+struct ModVersion {
+    version: Version,
+    // TODO: Use a HashSet for quick lookup?
+    dependencies: Vec<ModDependency>,
+}
+
+struct ModDependency {
+    name: String,
+    version_req: VersionReq,
 }
 
 fn main() {
     let app = App::from_args();
 
-    println!("{:#?}\n{:#?}\n{:#?}", app.enable, app.disable, app.dir)
+    let set = ModsSet::new(&app.dir);
 }
