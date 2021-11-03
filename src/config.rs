41e33253d060929b1ea03e7c6582ff181acf4376
raiguard <caleb.heuer@gmail.com>
2021-11-02 21:14:49 -0600

    Don't create config file automatically

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,6 @@
 use directories::BaseDirs;
 use serde::{Deserialize, Serialize};
 use std::fs;
-use std::fs::File;
 use std::path::PathBuf;
 use thiserror::Error;
 
@@ -12,30 +11,27 @@ pub struct ConfigFile {
 
 impl ConfigFile {
     pub fn new(path: &Option<PathBuf>) -> Result<Option<Self>, ConfigFileErr> {
-        // Pass custom config path, or create the default config file it it doesn't exist
         let config_path: Option<PathBuf> = path.clone().or({
             if let Some(base_dirs) = BaseDirs::new() {
                 let mut config_path: PathBuf = base_dirs.config_dir().into();
                 config_path.push("fmm");
-                if !config_path.exists() {
-                    fs::create_dir_all(&config_path).map_err(|_| ConfigFileErr::CreatePath)?;
-                }
                 config_path.push("fmm.toml");
-                if !config_path.exists() {
-                    config_path.push("fmm.toml");
-                    File::create(&config_path).map_err(|_| ConfigFileErr::CreateFile)?;
-                };
-                Some(config_path)
+
+                if config_path.exists() {
+                    Some(config_path)
+                } else {
+                    None
+                }
             } else {
                 None
             }
         });
+
         if config_path.is_none() {
             return Ok(None);
         }
 
-        let file =
-            std::fs::read_to_string(config_path.unwrap()).map_err(|_| ConfigFileErr::Open)?;
+        let file = fs::read_to_string(config_path.unwrap()).map_err(|_| ConfigFileErr::Open)?;
 
         let config: ConfigFile = toml::from_str(&file).map_err(|_| ConfigFileErr::ParseFile)?;
         Ok(Some(config))
@@ -44,10 +40,6 @@ impl ConfigFile {
 
 #[derive(Debug, Error)]
 pub enum ConfigFileErr {
-    #[error("Could not create config file.")]
-    CreateFile,
-    #[error("Could not create config file path.")]
-    CreatePath,
     #[error("Could not open config file.")]
     Open,
     #[error("Could not parse config file.")]
