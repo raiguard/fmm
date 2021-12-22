3c2f8f6f3081c32ded55dd857ff5d2850e8d4e69
raiguard <caleb.heuer@gmail.com>
2021-12-21 18:04:36 -0700

    Read startup settings

--- src/sync.rs
+++ src/sync.rs
@@ -1,8 +1,10 @@
+use anyhow::anyhow;
 use anyhow::Result;
 use byteorder::{LittleEndian, ReadBytesExt};
 use compress::zlib;
 use semver::Version;
 use semver::VersionReq;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Cursor;
@@ -22,7 +24,10 @@ pub struct SaveFile {
     pub path: PathBuf,
     pub scenario_mod_name: String,
     pub scenario: String,
+    pub startup_settings: PropertyTree,
 }
+
+pub type SaveFileReader = Cursor<Vec<u8>>;
 
 impl SaveFile {
     pub fn from(path: PathBuf) -> Result<Self> {
@@ -75,6 +80,12 @@ impl SaveFile {
             mods.push(read_mod(&mut cursor)?);
         }
 
+        cursor.seek(SeekFrom::Current(4));
+
+        let startup_settings = PropertyTree::load(&mut cursor)?;
+
+        println!("{:#?}", startup_settings);
+
         Ok(Self {
             mods,
             map_version: Version::new(
@@ -85,6 +96,7 @@ impl SaveFile {
             path,
             scenario: scenario_name,
             scenario_mod_name,
+            startup_settings,
         })
     }
 }
@@ -95,7 +107,61 @@ pub enum SaveFileErr {
     NoLevelDat,
 }
 
-fn read_string(reader: &mut Cursor<Vec<u8>>) -> Result<String> {
+#[derive(Debug)]
+pub enum PropertyTree {
+    None,
+    Boolean(bool),
+    Number(f64),
+    String(Option<String>),
+    List(Vec<PropertyTree>),
+    Dictionary(HashMap<String, PropertyTree>),
+}
+
+impl PropertyTree {
+    pub fn load(reader: &mut SaveFileReader) -> Result<Self> {
+        let pt_type = reader.read_u8()?;
+        // Internal flag that we don't have to care about
+        reader.seek(SeekFrom::Current(1))?;
+        match pt_type {
+            0 => Ok(Self::None),
+            1 => Ok(Self::Boolean(read_bool(reader)?)),
+            2 => Ok(Self::Number(reader.read_f64::<LittleEndian>()?)),
+            3 => Ok(Self::String(read_pt_string(reader)?)),
+            4 => {
+                let length = reader.read_u32::<LittleEndian>()?;
+                let mut list = Vec::with_capacity(length as usize);
+                for _ in 0..length {
+                    read_pt_string(reader)?;
+                    list.push(PropertyTree::load(reader)?);
+                }
+                Ok(Self::List(list))
+            }
+            5 => {
+                let length = reader.read_u32::<LittleEndian>()?;
+                let mut dict = HashMap::with_capacity(length as usize);
+                for _ in 0..length {
+                    dict.insert(
+                        read_pt_string(reader)?
+                            .ok_or_else(|| anyhow!("Missing key in PropertyTree Dictionary"))?,
+                        PropertyTree::load(reader)?,
+                    );
+                }
+                Ok(Self::Dictionary(dict))
+            }
+            _ => Err(anyhow!("Invalid data type in PropertyTree: {}", pt_type)),
+        }
+    }
+}
+
+fn read_bool(reader: &mut SaveFileReader) -> Result<bool> {
+    match reader.read_u8()? {
+        0 => Ok(false),
+        1 => Ok(true),
+        _ => Err(anyhow!("Invalid boolean representation in PropertyTree.")),
+    }
+}
+
+fn read_string(reader: &mut SaveFileReader) -> Result<String> {
     let scenario_len = reader.read_u8()?;
     let mut scenario_name = vec![0; scenario_len as usize];
     reader.read_exact(&mut scenario_name)?;
@@ -103,7 +169,16 @@ fn read_string(reader: &mut Cursor<Vec<u8>>) -> Result<String> {
     Ok(String::from_utf8_lossy(&scenario_name).to_string())
 }
 
-fn read_u16_optimized(reader: &mut Cursor<Vec<u8>>) -> Result<u16> {
+// Strings in PropertyTrees have an extra byte to inform us if they are empty
+fn read_pt_string(reader: &mut SaveFileReader) -> Result<Option<String>> {
+    if read_bool(reader)? {
+        Ok(None)
+    } else {
+        read_string(reader).map(Some)
+    }
+}
+
+fn read_u16_optimized(reader: &mut SaveFileReader) -> Result<u16> {
     let mut version: u16 = reader.read_u8()?.into();
     if version == 255 {
         version = reader.read_u16::<LittleEndian>()?;
@@ -111,15 +186,15 @@ fn read_u16_optimized(reader: &mut Cursor<Vec<u8>>) -> Result<u16> {
     Ok(version)
 }
 
-fn read_mod(reader: &mut Cursor<Vec<u8>>) -> Result<ModIdent> {
+fn read_mod(reader: &mut SaveFileReader) -> Result<ModIdent> {
     let mod_name = read_string(reader)?;
 
     let version_major = read_u16_optimized(reader)?;
     let version_minor = read_u16_optimized(reader)?;
     let version_patch = read_u16_optimized(reader)?;
 
-    // TODO: Figure out what these bytes are
-    reader.seek(SeekFrom::Current(4))?;
+    // We don't care about the CRC
+    let _crc = reader.read_u32::<LittleEndian>()?;
 
     Ok(ModIdent {
         name: mod_name,
