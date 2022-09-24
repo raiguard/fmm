046e570ffa66beb80192a2997f52f150907181e7
Caleb Heuer <ch@raiguard.me>
2022-09-24 15:40:16 -0600

    Update help text

--- src/main.rs
+++ src/main.rs
@@ -23,32 +23,30 @@ use std::path::PathBuf;
 use std::str::FromStr;
 use version::Version;
 
-const HELP: &str = "usage: fmm <options> <subcommand>
-environment variables:
-    FMM_TOKEN          oauth token for the mod portal
+const HELP: &str = "usage: fmm [options] <operation>
+operations:
+    {clean, c}                remove out-of-date mod versions, leaving only the newest version; ignores symlinked mods
+    {disable, d}     [mods]   disable the given mods, or all mods if no mods are given
+    {download, dl}   [mods]   download the given mods
+    {enable, e}      [mods]   enable the given mods
+    {enable-set, es} <set>    enable the mods from the given mod set
+    {query, q}       [mods]   query the local mods folder
+    {remove, r}      [mods]   remove the given mods
+    {search, l}      <query>  search for mods on the mod portal
+    {sync, s}        [mods]   enable the given mods, downloading if necessary, and disable all other mods
+    {sync-file, sf}  <path>   enable the mods from the given save file, downloading if necessary, disable all other mods, and sync mod startup settings
+    {sync-list, sl}  <path>   enable the mods from the given mod-list.json, downloading if necessary, and disable all other mods
+    {sync-set, ss}   <set>    enable the mods from the given mod set, downloading if necessary, and disable all other mods
+    {update, u}      [mods]   update the given mods, or all mods if no mods are given
+    {upload, ul}     <path>   upload the given mod zip file to the mod portal
 options:
-    --all              when using the clean subcommand, remove all non-symlink mods
+    --all              when using the clean operation, remove all non-symlink mods
     --config <PATH>    path to a custom configuration file
     --force -f         always download, overwriting an existing mod if necessary
     --game-dir <PATH>  path to the game directory
     --mods-dir <PATH>  path to the mods directory
-    --nodisable -d     when using sync subcommands, keep current mods enabled
-    --token <TOKEN>    oauth token for the mod portal
-subcommands:
-    {clean, c}                remove out-of-date mod versions, leaving only the newest version; ignores symlinked mods
-    {disable, d}     <MODS>   disable the given mods, or all mods if no mods are given
-    {download, dl}   <MODS>   download the given mods
-    {enable, e}      <MODS>   enable the given mods
-    {enable-set, es} <SET>    enable the mods from the given mod set
-    {query, q}       <MODS>   query the local mods folder
-    {remove, r}      <MODS>   remove the given mods
-    {search, l}      <QUERY>  search for mods on the mod portal
-    {sync, s}        <MODS>   enable the given mods, downloading if necessary, and disable all other mods
-    {sync-file, sf}  <PATH>   enable the mods from the given save file, downloading if necessary, disable all other mods, and sync mod startup settings
-    {sync-list, sl}  <PATH>   enable the mods from the given mod-list.json, downloading if necessary, and disable all other mods
-    {sync-set, ss}   <SET>    enable the mods from the given mod set, downloading if necessary, and disable all other mods
-    {update, u}      <MODS>   update the given mods, or all mods if no mods are given
-    {upload, ul}     <PATH>   upload the given mod zip file to the mod portal";
+    --nodisable -d     when using sync operations, keep current mods enabled
+    --token <TOKEN>    oauth token for the mod portal";
 
 pub fn main() -> Result<()> {
     let mut args = Arguments::from_env();
