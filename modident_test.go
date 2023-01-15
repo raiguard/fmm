bb0632615e8413a34cf2053c6201bb37ef65780e
Caleb Heuer <ch@raiguard.me>
2023-01-14 21:47:44 -0700

    Fix TestModIdent

--- modident_test.go
+++ modident_test.go
@@ -1,31 +1,29 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestModident(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
+
+func TestModIdent(t *testing.T) {
 	tests := []struct {
-		input    string
-		expected ModIdent
+		input, output string
+		expected      ModIdent
 	}{
-		{"Zipped", ModIdent{"Zipped", nil}},
-		{"Zipped 1.0.0", ModIdent{"Zipped", &Version{1}}},
-		{"Recipe_Book 1.0.35", ModIdent{"Recipe_Book", &Version{1, 0, 35}}},
+		{"Zipped", "Zipped", ModIdent{"Zipped", nil}},
+		{"Zipped_1.0.0", "Zipped 1.0.0", ModIdent{"Zipped", &Version{1}}},
+		{"Recipe_Book_1.0.35.zip", "Recipe_Book 1.0.35", ModIdent{"Recipe_Book", &Version{1, 0, 35}}},
 	}
 	for _, test := range tests {
 		mod := newModIdent(test.input)
-		if mod.Name != test.expected.Name {
-			t.Error("Mod name mismatch:", test.input, mod, test.expected)
-		}
+		assert.Equal(t, mod.Name, test.expected.Name)
 		if test.expected.Version != nil {
-			if mod.Version == nil || test.expected.Version.cmp(*mod.Version) != VersionEq {
-				t.Error("Mod version mismatch:", test.input, mod, test.expected)
-			}
-		} else if mod.Version != nil {
-			t.Error("Mod version mismatch:", test.input, mod, test.expected)
+			assert.NotNil(t, mod.Version)
+			assert.Equal(t, test.expected.Version.cmp(*mod.Version), VersionEq)
+		} else {
+			assert.Nil(t, mod.Version)
 		}
-		modStr := mod.toString()
-		if modStr != test.input {
-			t.Error("Mod string mismatch:", test.input, modStr)
-		}
+		assert.Equal(t, mod.toString(), test.output)
 	}
 }
